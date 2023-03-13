4a9f93cce381db242760e9f5d3ba17cb2d94a6ea
Hasnaa319337 <hasnaa3193378@gmail.com>
2023-03-13 14:16:33 +0200

    c

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="this.$i18n.locale == 'ar' ? 'is-rtl' : 'is_ltr'">
-    <sub-header />
+    <!-- <sub-header /> -->
     <HeaderComponent />
     <main>
       <router-view
@@ -25,9 +25,9 @@ import FooterComponent from "./components/FooterComponent.vue";
 
 import HomeView from "./views/HomeView.vue";
 import HeaderComponent from "@/components/HeaderComponent.vue";
-import SubHeader from "./components/SubHeader.vue";
+// import SubHeader from "./components/SubHeader.vue";
 export default {
-  components: { HomeView, FooterComponent, HeaderComponent, SubHeader },
+  components: { HomeView, FooterComponent, HeaderComponent },
 };
 </script>
 <style lang="scss"></style>
